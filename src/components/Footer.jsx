db4c676249e76326cf456a000482fb3e52cd2f0b
RESHMA-0727 <vishnumolakalareshmasri@gmail.com>
2026-06-14 16:52:07 +0530

    Navbar & Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+const Footer = ()=>{
+
+  return(
+    <>
+      <div className="footer">
+        <div className="site">
+          <h2>Cartify</h2>
+        </div>
+        <div className="categories">
+          <h2>Products</h2>
+          <ul>
+            <li>Electronics</li>
+            <li>Watches</li>
+            <li>Fashion</li>
+            <li>Shoes</li>
+          </ul>
+        </div>
+        <div className="contact">
+          <h2>Contact</h2>
+          <ul>
+            <li>Bengaluru</li>
+            <li>+91 9856457895</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Footer;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = ()=>{
+  return(
+    <>
+      <div className="nav">
+        <div className="left">
+          <h2>Logo</h2>
+        </div>
+        <div className="right">
+          <h2>Products</h2>
+          <h2>Cart</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+export default NavBar;
